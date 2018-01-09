095373e4bfe6f477d18f6e534ded77eb0bcb4551
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-01-09 09:49:35 +0530

    Positive,negative or Zero

---
+++ Beginner/Set1/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n<0)
+    {
+        printf("Negative");
+    }
+        if(n>0)
+    {
+        printf("Positive");
+    }
+            if(n==0)
+    {
+        printf("Zero");
+    }
+    
+    return 0;
+}
