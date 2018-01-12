0040e376144e7335edacd67aca247fd22f9bbd5c
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-01-12 17:59:24 +0530

    Leapyear or not

---
+++ Beginner/Set1/6.java
@@ -0,0 +1,27 @@
+package leapyearornot;
+import java.util.Scanner;
+public class Leapyearornot {
+    public static void main(String[] args) {
+       int year = 2018;
+        boolean leap = false;
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+    } 
