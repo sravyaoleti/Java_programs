6479876d383f91b7d33f646ea50890682cdcaa1c
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-01-20 18:44:10 +0530

    Maximum in array

---
+++ Beginner/set3/22.java
@@ -0,0 +1,16 @@
+package max;
+import java.util.*;
+public class Max {
+    public static void main(String[] args) {
+      Scanner a=new Scanner(System.in);
+      System.out.println("Enter N and elements");
+      int n=a.nextInt();
+      int arr[]=new int[n];
+      for(int i=0;i<n;i++)
+      {
+          arr[i]=a.nextInt();
+      }
+      Arrays.sort(arr);
+      System.out.println(arr[n-1]);
+    }
+}
