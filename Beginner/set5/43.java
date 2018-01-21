faa9dc8f599f16ee5c81807d523ef61e99f5376d
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-01-21 21:40:20 +0530

    concat

---
+++ Beginner/set5/43.java
@@ -0,0 +1,21 @@
+
+package pkg41_string_print;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        System.out.println("Enter 2 strings");
+        String in=a.nextLine();
+        String in1=a.nextLine();
+        System.out.println(in+in1);
+        
+    }
+    
+}
+
