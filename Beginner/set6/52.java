7284b818fe0df40f9d10b0d767519f9e7a6bd4b7
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-02 11:25:48 +0530

    N to words

---
+++ Beginner/set6/52.java
@@ -0,0 +1,42 @@
+package javaapplication2;
+import java.util.*;
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int no=sc.nextInt();
+        switch(no)
+        {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+                case 8:
+                System.out.println("Eight");
+                break;
+                case 9:
+                System.out.println("Nine");
+                break;
+                case 10:
+                System.out.println("Ten");
+                break;
+        }
+    } 
+}
