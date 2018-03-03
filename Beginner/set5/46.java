455b57571abf2b7fa68da13ecaeec67c78c8c3cd
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-03 20:48:50 +0530

    nextdig

---
+++ Beginner/set5/46.java
@@ -0,0 +1,10 @@
+package pkg44.java;
+import java.util.*;
+//System.out.println();
+public class Java {
+    public static void main(String[] args) {
+         Scanner a=new Scanner(System.in);
+         int no=a.nextInt();
+         System.out.println(no+1);
+    } 
+}
