f008ef264e0fb9d4ee769db42e71bb63608cb458
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-03 22:52:16 +0530

    7div

---
+++ Beginner/set7/68.java
@@ -0,0 +1,17 @@
+package pkg44.java;
+import java.util.*;
+//System.out.println();
+public class Java {
+    public static void main(String[] args) {
+         Scanner a=new Scanner(System.in);
+         int n=a.nextInt();
+         if(n%7==0)
+         {
+             System.out.println("Yes");
+         }
+         else
+         {
+             System.out.println("No");
+         }
+    }  
+}
