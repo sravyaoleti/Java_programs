66f3262aa46bd04c389a06cdec7074c725f6dfc9
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-03 22:03:28 +0530

    sum2nos

---
+++ Beginner/set5/49.java
@@ -0,0 +1,11 @@
+package pkg44.java;
+import java.util.*;
+//System.out.println();
+public class Java {
+    public static void main(String[] args) {
+         Scanner a=new Scanner(System.in);
+         int no=a.nextInt();
+         int no1=a.nextInt();
+         System.out.println(no+no1);
+    } 
+}
