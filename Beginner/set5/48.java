d61a8775a51298d3b421e94f3d1543e6bad9403b
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-03 22:00:20 +0530

    sumarrayelements

---
+++ Beginner/set5/48.java
@@ -0,0 +1,21 @@
+package pkg44.java;
+import java.util.*;
+//System.out.println();
+public class Java {
+    public static void main(String[] args) {
+         Scanner a=new Scanner(System.in);
+         int no=a.nextInt();
+         int[] ar=new int[no];
+         for(int i=0;i<no;i++)
+         {
+             ar[i]=a.nextInt();
+         }
+         int sum=0;
+         for(int i=0;i<no;i++)
+         {
+            sum+=ar[i]; 
+         }
+
+            System.out.print(sum);             
+    } 
+}
