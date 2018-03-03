6540ffb74ea1c0279ac7ccb3ba804b31b0f4671a
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-03 23:23:14 +0530

    ceil

---
+++ Beginner/set8/74.java
@@ -0,0 +1,11 @@
+package pkg44.java;
+import java.util.*;
+import java.lang.*;
+//System.out.println();
+public class Java {
+    public static void main(String[] args) {
+         Scanner a=new Scanner(System.in);
+         float x=a.nextFloat();
+         System.out.println(Math.ceil(x));   
+    }  
+}
