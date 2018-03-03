75474ee4db6b706ec4579fd585ae01db48799f56
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-03 22:43:12 +0530

    diff_odd_even

---
+++ Beginner/set7/69.java
@@ -0,0 +1,19 @@
+package pkg44.java;
+import java.util.*;
+//System.out.println();
+public class Java {
+    public static void main(String[] args) {
+         Scanner a=new Scanner(System.in);
+         int n=a.nextInt();
+         int m=a.nextInt();
+         int r=n-m;
+         if(r%2==0)
+         {
+             System.out.println("Even");
+         }
+         else
+         {
+             System.out.println("Odd");
+         }
+    }  
+}
