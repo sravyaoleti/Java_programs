5098ab033651c950f7387edaaa010e1634957aa7
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-03 20:43:55 +0530

    range

---
+++ Beginner/set5/44.java
@@ -0,0 +1,17 @@
+package pkg44.java;
+import java.util.*;
+
+public class Java {
+    public static void main(String[] args) {
+         Scanner a=new Scanner(System.in);
+         int no=a.nextInt();
+         if(no>=1 && no<=10)
+         {
+             System.out.println("yes");
+         }
+         else
+         {
+             System.out.println("no");
+         }
+    } 
+}
