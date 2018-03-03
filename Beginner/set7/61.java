6717688e460917e1b383c2f008a6748d1bb30cbc
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-03 22:10:35 +0530

    Stringindex

---
+++ Beginner/set7/61.java
@@ -0,0 +1,14 @@
+package pkg44.java;
+import java.util.*;
+//System.out.println();
+public class Java {
+    public static void main(String[] args) {
+         Scanner a=new Scanner(System.in);
+         String n=a.next();
+         int x=a.nextInt();
+         for(int i=0;i<x;i++)
+         {
+             System.out.print(n.charAt(i));
+         }
+    } 
+}
