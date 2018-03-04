2ffeb49b85f20a2f91f09746f480a2d8091e8b88
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-04 23:06:55 +0530

    attachnos

---
+++ Beginner/set12/116.java
@@ -0,0 +1,10 @@
+package javaapplication46;
+import java.util.Scanner;
+public class JavaApplication46 {
+    public static void main(String[] args) {
+          Scanner a=new Scanner(System.in);
+          int n=a.nextInt();
+          int k=a.nextInt();
+          System.out.println(n+""+k);
+    }  
+}
