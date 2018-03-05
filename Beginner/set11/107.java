cfeb625416ac05a019928198d1f7c906016715a3
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-05 06:36:03 +0530

    a*b/c

---
+++ Beginner/set11/107.java
@@ -0,0 +1,12 @@
+package javaapplication46;
+import java.util.Scanner;
+public class JavaApplication46 {
+    public static void main(String[] args) {
+          Scanner a=new Scanner(System.in);
+          int a1=a.nextInt();
+          int b=a.nextInt();
+          int c=a.nextInt();
+          int r=(a1*b)/c;
+          System.out.println(r);
+    }  
+}
