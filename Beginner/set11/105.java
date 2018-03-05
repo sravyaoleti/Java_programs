92d98bbf0b4eff3b07f9b24cb7c10c3a30a641b3
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-05 06:32:04 +0530

    alphabets

---
+++ Beginner/set11/105.java
@@ -0,0 +1,11 @@
+package javaapplication47;
+import java.util.Scanner;
+import java.lang.*;
+
+public class JavaApplication47 {
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        int b=a.nextInt();
+        System.out.printf("%c", 64+b);
+    }   
+}
