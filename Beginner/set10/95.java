d675e10014d2327dd5440dbc99ca0e0067924e3a
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-07 06:33:51 +0530

    SI

---
+++ Beginner/set10/95.java
@@ -0,0 +1,11 @@
+package javaapplication49;
+import java.util.Scanner;
+public class JavaApplication49 {
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       int p=s.nextInt();
+       int t=s.nextInt();
+       int r=s.nextInt();
+       System.out.println(((p*t*r)/100));
+    }
+}
