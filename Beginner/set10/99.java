cb297debd42f0f863a821c233b7f3e103f35cd40
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-07 06:37:55 +0530

    a*b%c

---
+++ Beginner/set10/99.java
@@ -0,0 +1,11 @@
+package javaapplication49;
+import java.util.Scanner;
+public class JavaApplication49 {
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       int a=s.nextInt();
+       int b=s.nextInt();
+       int c=s.nextInt();
+       System.out.println(((a*b)%c));
+    }
+}
