539d124caf4d879a735e498d4ff0316f18e7689e
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-07 06:24:22 +0530

    c_k

---
+++ Beginner/set10/93.java
@@ -0,0 +1,9 @@
+package javaapplication49;
+import java.util.Scanner;
+public class JavaApplication49 {
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       int celsius=s.nextInt();
+       System.out.println(celsius+273);
+    }
+}
