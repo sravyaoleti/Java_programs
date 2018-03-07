2c158e10d104ed8d84af60bfe008b081a7a74912
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-03-07 20:27:23 +0530

    sum_natural_no

---
+++ Beginner/set11/110.java
@@ -0,0 +1,15 @@
+package javaapplication50;
+
+import java.util.Scanner;
+
+public class JavaApplication50 {
+
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+       long n=s.nextLong();
+       long sn=(n*(n+1)/2);
+        System.out.println(sn);
+    }
+    
+}
