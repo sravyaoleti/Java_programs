a27f203b3b43eca1b22f32e1494abab253eb9190
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-05-25 06:40:03 +0530

    odd factors

---
+++ Player/SET5/48.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0 && i%2==1)
+            {
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
