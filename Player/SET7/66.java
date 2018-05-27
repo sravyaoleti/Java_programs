58128a598f2086387db8b8072ec848efbcb9ba93
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-05-27 12:52:32 +0530

    k==count

---
+++ Player/SET7/66.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,k,count=0;
+		n=sc.nextInt();
+		k=sc.nextInt();
+		
+		int[] a=new int[n];
+		
+		for(int i=0;i<n;i++)
+		    a[i]=sc.nextInt();
+		    
+		for(int i=0;i<n;i++)
+		{
+		    count=0;
+		    for(int j=i;j<n;j++)
+		    {
+		        if(a[i]==a[j])
+		        {
+		            count++;
+		        }
+		    }
+		    if(count==k)
+		    {
+		        System.out.println(a[i]);
+		        break;
+		    }
+		}
+		
+	}
+}
