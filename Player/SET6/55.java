a98ef7150448ae18a83446625d51ee9cfb81b0fe
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-05-27 20:56:04 +0530

    Update 55.java

--- Player/SET6/55.java
+++ Player/SET6/55.java
@@ -1,19 +1,15 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		String a,b;
+		String a,b,x,y;
 		int f=0;
-		a=sc.next();
-		b=sc.next();
+		x=sc.next();
+		y=sc.next();
 		a=a.toLowerCase();
 		b=b.toLowerCase();
 		
