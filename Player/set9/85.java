72461030aee8322cebd92bafb44bb783564f4206
haripranesh <35248315+haripranesh@users.noreply.github.com>
2018-06-11 21:27:37 +0530

    not~

---
+++ Player/set9/85.java
@@ -0,0 +1,13 @@
+package bitwise_and;
+import java.util.*;
+public class Bitwise_and {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int n,max=0;
+		n=sc.nextInt();
+		int[] a=new int[n];
+		max=~n;
+		System.out.println(max);
+    } 
+}
